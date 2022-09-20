79c72307a218d98192a758813e11c6b5deb9b84d
ouzhsh-GZHU <113835528+ouzhsh-GZHU@users.noreply.github.com>
2022-09-20 23:30:51 +0800

    Update 6-sendDataStorageReq.js

--- DSU-API/6-sendDataStorageReq.js
+++ DSU-API/6-sendDataStorageReq.js
@@ -9,17 +9,10 @@ var myContract = new web3.eth.Contract(abi, contractAddr)
 var pk = ' '
 var sk = ' '
 
-
 var encData = ' '
 var serialNumber = ' '
 
-
-
-
 function sendStorageReq(serialNumber) {
-
-
-
 	myContract.methods.DSCs(serialNumber).call({
 		from: pk,
 		gas: 3000000,
@@ -27,17 +20,11 @@ function sendStorageReq(serialNumber) {
 
 		console.log('DSCres', DSCres)
 
-
 		var myJSON = JSON.stringify(DSCres)
 		console.log('myJSON', myJSON)
 
-
 		var signature = web3.eth.accounts.sign(myJSON, sk).signature
 		console.log('signature', signature)
-
-
-
-
 	})
 
 }
